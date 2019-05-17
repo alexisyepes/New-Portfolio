578726445419d13dccaa809499cc4d6339c11335
Alex Yepes <alexisyepes@yahoo.com>
2019-05-16 21:37:10 -0400

    Updates

---
+++ precache-manifest.ac3ef0e2c5df30f9a676341460c611d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bcbcb452994b384a6b474f48790c84",
+    "url": "/alexisyepes/New-Portfolio/index.html"
+  },
+  {
+    "revision": "71ca449232c7d485b649",
+    "url": "/alexisyepes/New-Portfolio/static/css/main.7f4c7c15.chunk.css"
+  },
+  {
+    "revision": "1d2dd2f8dbf2a7771020",
+    "url": "/alexisyepes/New-Portfolio/static/js/2.7b216c48.chunk.js"
+  },
+  {
+    "revision": "71ca449232c7d485b649",
+    "url": "/alexisyepes/New-Portfolio/static/js/main.d32311c6.chunk.js"
+  },
+  {
+    "revision": "7cb7d74bd85d3680933a",
+    "url": "/alexisyepes/New-Portfolio/static/js/runtime~main.dcde857c.js"
+  }
+]);
