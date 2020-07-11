4254777813ecb3e3d8fbe428f2984a87f0cef1a6
alexisysanabria <alexisyep@gmail.com>
2020-07-11 14:05:59 -0400

    new version

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,62 +1,80 @@
-import React, { Component } from 'react';
-import './style.css';
+import React, { Component } from "react";
+import "./style.css";
 
 class About extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row ">
-                    <article>
-                        <div className="col-md-12 aboutBox">
-                            <h1 className="aboutTitle">About Me</h1>
-                            <article>
-                            <img className="pic" alt="my pic" src="https://res.cloudinary.com/bootcamp2019/image/upload/v1557632591/photoOfMeSmall.jpg"></img>
-                            <p className="aboutParag">
-                                My name is Alexis Yepes Sanabria, a Full Stack Developer with a background in graphic design.
-                              I'm passionate about music and computers, and love solving problems. Fully bilingual in English/Spanish
-                            </p>
-                            </article>
-                            <br></br>
-                            <hr></hr>
-                            <br></br>
-                            <div className="iconsBox">
-                                <h1 className="skillsTitle">SKILLS<span><hr className="lineUnderSkills" style={{ background: "black" }}></hr>
-                                    <p className="skillsParag">Demostrated experience in:<br /><br /> HTML5, CSS3, Javascript, JQuery,
-                                        Node.Js, React, Bootstrap, MySql, MongoDB, Github, Photoshop, Illustrator, Express, Handlebars.Js,
-                                Firebase, Ajax, MVC pattern, Git version control</p></span></h1>
-
-                                <img className="aboutIcons" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834672/html.png"></img>
-                                <img className="aboutIconsCss" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,q_auto:best,w_120/v1557834671/css.png"></img>
-                                <img className="aboutIconsJs" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834671/javascript.png"></img>
-                                <img className="aboutIconsJq" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834672/jquery.png"></img>
-                                <img className="aboutIconsNode" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834684/node.png"></img>
-                                <img className="aboutIconsReact" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834699/react.png"></img>
-                                <img className="aboutIconsBootstrap" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834671/bootstrap.png"></img>
-                                <img className="aboutIconsMysql" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834672/mysql.png"></img>
-                                <img className="aboutIconsMongodb" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834671/mongodb.png"></img>
-                                <img className="aboutIconsGithub" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_100,w_120/v1557835506/GitHub.png"></img>
-                                <img className="aboutIconsPs" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,q_auto:best,w_120/v1557834691/photoshop.png"></img>
-                                <img className="aboutIconsIll" alt="html" src="https://res.cloudinary.com/bootcamp2019/image/upload/c_scale,h_70,w_120/v1557834671/illustrator.png"></img>
-                                <div className="col-md-12" id="connectBox">
-                                    <h3><b>Connect with me</b></h3>
-                                    <section className="row">
-                                        <div className="col-sm-6">
-                                            <a href="https://github.com/alexisyepes" target="blank"><img alt="github" src="/images/GitHub.png"
-                                                id="connectIcon1About" /></a>
-                                        </div>
-                                        <div className="col-sm-6">
-                                            <a href="https://www.linkedin.com/in/alexis-sanabria-80002574/" target="blank"><img alt="linkedin" src="/images/linkedIn.png"
-                                                id="connectIcon2About" /></a>
-                                        </div>
-                                    </section>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
+  render() {
+    return (
+      <div className="container about-container">
+        <div className="row ">
+          <article>
+            <div className="col-md-12 aboutBox">
+              <h1 className="aboutTitle">Alex Y Sanabria</h1>
+              <article>
+                <img
+                  className="pic"
+                  alt="my pic"
+                  src="https://res.cloudinary.com/bootcamp2019/image/upload/v1557632591/photoOfMeSmall.jpg"
+                ></img>
+                <p className="aboutParag">
+                  My name is Alexis Yepes Sanabria, a Full Stack Developer from
+                  Cambridge, Ontario, with a background in graphic design. I'm
+                  passionate about music and computers, and love solving
+                  problems. <br />
+                  I'm fully bilingual in English and Spanish
+                </p>
+              </article>
+              <br></br>
+              <hr></hr>
+              <br></br>
+              <div className="iconsBox">
+                <div className="skills-wrapper">
+                  <h1 className="skillsTitle">Skills</h1>
+                  <h3 className="skills-parag-subtitle">
+                    <i className="fas fa-laptop-code"></i> I have experience
+                    with these technologies :
+                  </h3>
+                  <p className="skillsParag">
+                    React Js, Node Js, HTML5, CSS3, SASS, Javascript, GraphQl,
+                    MySql, MongoDB, Nodemailer, Bootstrap, Photoshop,
+                    Illustrator, Express Js, Handlebars.Js, Firebase, Heroku,
+                    Ajax, MVC pattern, Git version control
+                  </p>
+                  <div className="col-md-12" id="connectBox">
+                    <h3>
+                      <b>Connect with me</b>
+                    </h3>
+                    <section className="row">
+                      <div className="col-sm-6">
+                        <a href="https://github.com/alexisyepes" target="blank">
+                          <img
+                            alt="github"
+                            src="/images/GitHub.png"
+                            id="connectIcon1About"
+                          />
+                        </a>
+                      </div>
+                      <div className="col-sm-6">
+                        <a
+                          href="https://www.linkedin.com/in/alexis-sanabria-80002574/"
+                          target="blank"
+                        >
+                          <img
+                            alt="linkedin"
+                            src="/images/linkedIn.png"
+                            id="connectIcon2About"
+                          />
+                        </a>
+                      </div>
+                    </section>
+                  </div>
                 </div>
+              </div>
             </div>
-        )
-    }
+          </article>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
