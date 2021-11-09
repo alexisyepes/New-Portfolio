9787102b2414531d71ea22ef9d36bcae264e7c32
alexisysanabria <alexisyep@gmail.com>
2021-11-09 15:20:54 -0500

    Updated info in about

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,28 +2,28 @@ import React from "react";
 import "./style.css";
 
 export default function index() {
-  return (
-    <div className="contact-main-containerr">
-      <div className="container">
-        <div className="col-md-12" id="connectBoxContact">
-          <h3 className="connectTitle">
-            <b>Connect with me</b>
-          </h3>
-          <hr></hr>
-          <section className="row">
-            <div className="col-sm-12">
-              <a
-                className="connect-links"
-                href="https://github.com/alexisyepes"
-                target="blank"
-              >
-                <img alt="github" src="/images/GitHub.png" id="connectIcon1" />
-                <h3 style={{ color: "white" }} className="linkLabels">
-                  Github
-                </h3>
-              </a>
-            </div>
-            {/* <div className="col-sm-6">
+	return (
+		<div className="contact-main-containerr">
+			<div className="container">
+				<div className="col-md-12" id="connectBoxContact">
+					<h3 className="connectTitle">
+						<b>Connect with me</b>
+					</h3>
+					<hr></hr>
+					<section className="row">
+						<div className="col-sm-12">
+							<a
+								className="connect-links"
+								href="https://github.com/alexisyepes"
+								target="blank"
+							>
+								<img alt="github" src="/images/GitHub.png" id="connectIcon1" />
+								<h3 style={{ color: "white" }} className="linkLabels">
+									Github
+								</h3>
+							</a>
+						</div>
+						{/* <div className="col-sm-6">
               <a
                 className="connect-links"
                 href="https://www.linkedin.com/in/alexis-sanabria-80002574/"
@@ -39,30 +39,30 @@ export default function index() {
                 </h3>
               </a>
             </div> */}
-          </section>
-          <br></br>
-          <br></br>
-          <a href="mailto: alexisyep@gmail.com">
-            <h4 className="contact--info">
-              <b>
-                <i className="fas fa-envelope-square"></i> alexisyep@gmail.com
-              </b>
-            </h4>
-          </a>
-          <h4 className="contact--info">
-            <b>
-              <i className="fas fa-map-marker-alt"></i> Cambridge, ON
-            </b>
-          </h4>
-          <br></br>
-          <hr></hr>
-          {/* <a href="/images/new-resume-2021.pdf" target="blank">
-            <h4 style={{ color: "white", fontSize: 25 }}>
-              <b>Click to view resume</b>
-            </h4>
-          </a> */}
-        </div>
-      </div>
-    </div>
-  );
+					</section>
+					<br></br>
+					<br></br>
+					<a href="mailto: alexisyep@gmail.com">
+						<h4 className="contact--info">
+							<b>
+								<i className="fas fa-envelope-square"></i> alexisyep@gmail.com
+							</b>
+						</h4>
+					</a>
+					<h4 className="contact--info">
+						<b>
+							<i className="fas fa-map-marker-alt"></i> Cambridge, ON
+						</b>
+					</h4>
+					<br></br>
+					<hr></hr>
+					<a href="/images/new-resume-2021.pdf" target="blank">
+						<h4 style={{ color: "white", fontSize: 25 }}>
+							<b>Click to view resume</b>
+						</h4>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,32 +2,32 @@ import React, { Component } from "react";
 import "./style.css";
 
 export default class index extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 homeBox">
-            <h3 className="homeSubtitle">Alexis Yepes Sanabria</h3>
-            <hr style={{ width: "70%" }}></hr>
-            <h6 className="fullStackDev"> Full-Stack Developer </h6>
-            <div className="linkToAbout">
-              <a href="/about" style={{ textDecoration: "none" }}>
-                {" "}
-                <h6 className="homeAboutMeBox">More About Me</h6>
-              </a>
-            </div>
-            <br />
-            {/* <a
-              className="text-center"
-              href="/images/new-resume-2021.pdf"
-              target="blank"
-            >
-              <h4 style={{ color: "white", fontSize: 25 }}>
-                <b>Click to view resume</b>
-              </h4>
-            </a> */}
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12 homeBox">
+						<h3 className="homeSubtitle">Alexis Yepes Sanabria</h3>
+						<hr style={{ width: "70%" }}></hr>
+						<h6 className="fullStackDev"> Full-Stack Developer </h6>
+						<div className="linkToAbout">
+							<a href="/about" style={{ textDecoration: "none" }}>
+								{" "}
+								<h6 className="homeAboutMeBox">More About Me</h6>
+							</a>
+						</div>
+						<br />
+						<a
+							className="text-center"
+							href="/images/new-resume-2021.pdf"
+							target="blank"
+						>
+							<h4 style={{ color: "white", fontSize: 25 }}>
+								<b>Click to view resume</b>
+							</h4>
+						</a>
 
-            {/* <a
+						{/* <a
               className="text-center"
               href="https://www.aypwebcreations.com/"
               target="blank"
@@ -37,16 +37,16 @@ export default class index extends Component {
                 <b>Go to freelance Page</b>
               </button>
             </a> */}
-          </div>
-        </div>
-        <div className="bg-video">
-          <video className="bg-video__content" autoPlay muted>
-            <source src="./videos/letters.mp4" type="video/mp4" />
-            <source src="img/video.webm" type="video/webm" />
-            Your browser is not supported!
-          </video>
-        </div>
-      </div>
-    );
-  }
+					</div>
+				</div>
+				<div className="bg-video">
+					<video className="bg-video__content" autoPlay muted>
+						<source src="./videos/letters.mp4" type="video/mp4" />
+						<source src="img/video.webm" type="video/webm" />
+						Your browser is not supported!
+					</video>
+				</div>
+			</div>
+		);
+	}
 }
