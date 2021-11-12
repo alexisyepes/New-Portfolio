1288a5ee97832740e46e6e71963b15dfb0650a01
alexisysanabria <alexisyep@gmail.com>
2021-11-12 10:09:22 -0500

    removing broken project

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -4,7 +4,7 @@ import Project18 from "../ProjectFaces/Project18";
 import Project17 from "../ProjectFaces/Project17";
 import Project16 from "../ProjectFaces/Project16";
 import Project15 from "../ProjectFaces/Project15";
-import Project14 from "../ProjectFaces/Project14";
+// import Project14 from "../ProjectFaces/Project14";
 import Project13 from "../../components/ProjectFaces/Project13";
 import Project12 from "../../components/ProjectFaces/Project12";
 import Project11 from "../../components/ProjectFaces/Project11";
@@ -20,10 +20,10 @@ import Project2 from "../../components/ProjectFaces/Project2";
 import Project1 from "../../components/ProjectFaces/Project1";
 
 export default function index() {
-  return (
-    <div className=" boxWrapper">
-      <div className=" mainBox">
-        {/* <a
+	return (
+		<div className=" boxWrapper">
+			<div className=" mainBox">
+				{/* <a
           className="text-center nounderline"
           href="https://www.aypwebcreations.com/"
           target="blank"
@@ -33,41 +33,41 @@ export default function index() {
             <b>Visit freelance Page for real-life projects</b>
           </button>
         </a> */}
-        <h1 className="block-header">Portfolio</h1>
-        <div className="project-face-wrapper">
-          <Project18 />
-          <Project17 />
-          <Project16 />
-          <Project15 />
-          <Project14 />
-          <Project13 />
-          <Project12 />
-          <Project11 />
-          <Project10 />
-          <Project9 />
-          <Project8 />
-          <Project7 />
-          <Project6 />
-          <Project5 />
-          <Project4 />
-          <Project3 />
-          <Project2 />
-          <Project1 />
-          <div className="" id="connectBoxWork">
-            <h1 className="connectBoxWork">
-              <b>Connect with me</b>
-            </h1>
-            <section className="row">
-              <div className="col-sm-12">
-                <a href="https://github.com/alexisyepes" target="blank">
-                  <img
-                    alt="github"
-                    src="/images/GitHub.png"
-                    id="connectIcon1Work"
-                  />
-                </a>
-              </div>
-              {/* <div className="col-sm-6">
+				<h1 className="block-header">Portfolio</h1>
+				<div className="project-face-wrapper">
+					<Project18 />
+					<Project17 />
+					<Project16 />
+					<Project15 />
+					{/* <Project14 /> */}
+					<Project13 />
+					<Project12 />
+					<Project11 />
+					<Project10 />
+					<Project9 />
+					<Project8 />
+					<Project7 />
+					<Project6 />
+					<Project5 />
+					<Project4 />
+					<Project3 />
+					<Project2 />
+					<Project1 />
+					<div className="" id="connectBoxWork">
+						<h1 className="connectBoxWork">
+							<b>Connect with me</b>
+						</h1>
+						<section className="row">
+							<div className="col-sm-12">
+								<a href="https://github.com/alexisyepes" target="blank">
+									<img
+										alt="github"
+										src="/images/GitHub.png"
+										id="connectIcon1Work"
+									/>
+								</a>
+							</div>
+							{/* <div className="col-sm-6">
                 <a
                   href="https://www.linkedin.com/in/alexis-sanabria-80002574/"
                   target="blank"
@@ -79,10 +79,10 @@ export default function index() {
                   />
                 </a>
               </div> */}
-            </section>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
